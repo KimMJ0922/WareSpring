1bd9e93b08564fa2a4b37c6bdedfdd2a8ba3ceee
Unknown <zkxm_0@naver.com>
2020-06-08 17:24:09 +0900

    test

--- src/main/java/card/controller/CardController.java
+++ src/main/java/card/controller/CardController.java
@@ -1,9 +1,5 @@
 package card.controller;
 
-
-
-import javax.annotation.Resource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
