367f1383dbb9a8043e0830793c726a5ba947c518
Unknown <zkxm_0@naver.com>
2020-06-23 19:40:58 +0900

    객관식 끝

--- src/main/java/card/controller/CardController.java
+++ src/main/java/card/controller/CardController.java
@@ -1,6 +1,7 @@
 package card.controller;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import card.dto.CardDTO;
 import card.dto.CardSetDTO;
+import card.dto.ChoiceDTO;
 import card.service.CardService;
 import card.service.CardSetService;
 import member.dto.MemberDTO;
@@ -91,6 +93,70 @@ public class CardController {
 		map.put("csdto", csdto);
 		map.put("cList", cList);
 
+		return map;
+	}
+	
+	//카드 목록 가져오기
+	@PostMapping("/getcardchoicelist")
+	@ResponseBody
+	public Map<String,Object> getCardChoiceList(@RequestBody Map<String, Object> map){
+
+		//카드 리스트
+		List<CardDTO> cList = cs.getCardList(map);
+		List<ChoiceDTO> choiceList = new ArrayList<ChoiceDTO>();
+		for(CardDTO cdto : cList) {
+			int question_no = cdto.getQuestion_no();
+			//객관식 만들기 위한 배열
+			List<Integer> ranList = new ArrayList<Integer>();
+			ranList.add(question_no-1);
+			while(true) {
+				if(ranList.size() == 4) {
+					break;
+				}
+				double ran = Math.random()*cList.size();
+				int rand = (int)ran;
+				
+				if(ranList.indexOf(rand) == -1) {
+					ranList.add(rand);
+				}
+			}
+			
+			//랜덤하게 섞기
+			Collections.shuffle(ranList);
+			String t = "";
+			ChoiceDTO codto = new ChoiceDTO();
+			for(int i=0; i<ranList.size();i++) {
+				CardDTO cddto = cList.get(ranList.get(i));
+				switch (i) {
+				case 0:
+					codto.setAnswer1(cddto.getAnswer());
+					break;
+				case 1:
+					codto.setAnswer2(cddto.getAnswer());
+					break;
+				case 2:
+					codto.setAnswer3(cddto.getAnswer());
+					break;
+				case 3:
+					codto.setAnswer4(cddto.getAnswer());
+					break;
+				}
+			}
+			
+			choiceList.add(codto);
+		}
+		
+		//카드 세트의 정보
+		CardSetDTO csdto = css.getCardSet(map);
+
+		//카드 세트 만든 계정의 정보
+		MemberDTO mdto = ms.getMember(csdto.getMember_no());
+
+		map.put("mdto", mdto);
+		map.put("csdto", csdto);
+		map.put("cList", cList);
+		map.put("choiceList", choiceList);
+		
 		return map;
 	}
 }
